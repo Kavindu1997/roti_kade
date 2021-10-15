3956e1897417942ab456637d08975430613ae953
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-10-15 21:12:49 +0530

    syntax fix

--- index.php
+++ index.php
@@ -38,16 +38,16 @@
 	<div class="container">
 		<div class="row">
 
-			<?php foreach($pizzas as $pizza){ ?>
+			<?php foreach($pizzas as $pizza): ?>
 
 				<div class="col s6 md3">
 					<div class="card z-depth-0">
 						<div class="card-content center">
 							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
 							<ul class="grey-text">
-								<?php foreach(explode(',', $pizza['ingredients']) as $ing){ ?>
+								<?php foreach(explode(',', $pizza['ingredients']) as $ing): ?>
 									<li><?php echo htmlspecialchars($ing); ?></li>
-								<?php } ?>
+								<?php endforeach ?>
 							</ul>
 						</div>
 						<div class="card-action right-align">
@@ -56,7 +56,7 @@
 					</div>
 				</div>
 
-			<?php } ?>
+			<?php endforeach; ?>
 
 		</div>
 	</div>
